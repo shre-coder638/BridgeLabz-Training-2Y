7a8b04131461b6d42339f34b92fd35cbe5491b7a
SHREYASH SHANKAR <shankarshreyash79@gmail.com>
2025-08-19 11:12:20 +0530

    Update KilometersToMiles.java

--- KilometersToMiles.java
+++ KilometersToMiles.java
@@ -5,7 +5,7 @@ class KilometersToMiles {
      
         // Create a string variable name and assign value Sam
         double KM = 10.8;
-        double Miles = KM * 1.6;
+        double Miles = KM * 0.6;
         
        System.out.println("The distance " + KM + " km in miles is " + Miles);
     }
