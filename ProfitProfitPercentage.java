bf5eb1276e6797fdff096ccbbe2b4c86cf26fbf6
shre-coder638 <shankarshreyash79@gmail.com>
2025-08-19 10:49:18 +0530

    Profit & Profit percentage

---
+++ ProfitProfitPercentage.java
@@ -0,0 +1,15 @@
+// Creating Class with name DisplayResult indicating the purpose is to display 
+// result. Notice the class name is a Noun.
+class ProfitProfitPercentage {
+    public static void main(String[] args) {
+     
+        // Create a string variable name and assign value Sam
+        int cp = 129;
+        int sp = 191;
+        int profit = sp - cp;
+        double profitPercentage = (profit / (double)cp) * 100;
+
+       System.out.println("The Cost Price is INR " + cp + " and Selling Price is INR " + sp);
+       System.out.println("The profit is " + profit + " and the profit percentage is " + profitPercentage + "%");
+    }
+}
