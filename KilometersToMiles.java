1c805dbfee0a90102d3894bdf62932f6a72e0d8a
shre-coder638 <shankarshreyash79@gmail.com>
2025-08-19 10:44:10 +0530

    Kilometres to Miles

---
+++ KilometersToMiles.java
@@ -0,0 +1,12 @@
+// Creating Class with name DisplayResult indicating the purpose is to display 
+// result. Notice the class name is a Noun.
+class KilometersToMiles {
+    public static void main(String[] args) {
+     
+        // Create a string variable name and assign value Sam
+        double KM = 10.8;
+        double Miles = KM * 1.6;
+        
+       System.out.println("The distance " + KM + " km in miles is " + Miles);
+    }
+}
