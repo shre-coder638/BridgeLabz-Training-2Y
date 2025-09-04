efedb4391a02da9294607b76b1874939656792a0
SHREYASH SHANKAR <shankarshreyash79@gmail.com>
2025-09-04 10:48:59 +0530

    Clean up comments in Universityfee.java
    Removed comments and simplified the code structure.

--- Programing elements/Level 1/Universityfee.java
+++ Programing elements/Level 1/Universityfee.java
@@ -1,9 +1,6 @@
-// Creating Class with name DisplayResult indicating the purpose is to display 
-// result. Notice the class name is a Noun.
 class Universityfee {
     public static void main(String[] args) {
      
-        // Create a string variable name and assign value Sam
         int Fees = 125000;
         int discount = 10;
         int discountedamount = Fees * discount / 100;
