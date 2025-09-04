b32b778cab2def651604e881f88bc35e892803e5
SHREYASH SHANKAR <shankarshreyash79@gmail.com>
2025-09-04 10:46:07 +0530

    Delete Programing elements/Level 1/KilometersToMiles.java

--- Programing elements/Level 1/KilometersToMiles.java
+++
@@ -1,9 +0,0 @@
-class KilometersToMiles {
-    public static void main(String[] args) {
-     
-        // Create a string variable name and assign value Sam
-        double KM = 10.8;
-        double Miles = KM * 0.6;
-       System.out.println("The distance " + KM + " km in miles is " + Miles);
-    }
-}
